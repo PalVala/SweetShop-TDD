9a286cb2f184ecff0b7a6203f403ef97216e53dd
PalVala <valapal2005@gamil.com>
2025-07-17 05:32:52 +0530

    Test : deleting a sweet by ID & throwing error if ID doesn't exist

--- test/sweetshop.test.js
+++ test/sweetshop.test.js
@@ -1,8 +1,10 @@
 const { Sweet, sweets } = require('../model/sweet');
-const { addSweet } = require('../controller/sweetshop');
+const { addSweet,deleteSweet } = require('../controller/sweetshop');
 
 beforeEach(() => sweets.length = 0); 
 
+
+// adding a new sweet
 test('addSweet() should add a new sweet using Sweet class', () => {
   const sweetData = {
     id: 1,
@@ -17,4 +19,21 @@ test('addSweet() should add a new sweet using Sweet class', () => {
   expect(sweets.length).toBe(1);
   expect(sweets[0]).toMatchObject(sweetData);
   expect(result).toBeInstanceOf(Sweet);
+});
+
+
+// Delete existing sweet
+test('deleteSweet() should remove sweet by ID', () => {
+  const sweetData = { id: 1, name: 'Ladoo', category: 'Traditional', price: 25, quantity: 30 };
+  addSweet(sweetData);
+
+  const result = deleteSweet(1);
+
+  expect(result).toBe(true);
+  expect(sweets.length).toBe(0);
+});
+
+// Delete non-existent sweet
+test('deleteSweet() should throw error if ID not found', () => {
+  expect(() => deleteSweet(99)).toThrow('Sweet with ID 99 not found');
 });
