2c20694cb66dc1350f0f4eb625bbfbdb2901ab8b
PalVala <valapal2005@gamil.com>
2025-07-17 05:46:59 +0530

    Functionality : update properties of sweet

--- controller/sweetshop.js
+++ controller/sweetshop.js
@@ -22,11 +22,30 @@ function getAllSweets() {
   return sweets;
 }
 
+function updateSweet(id, newData) {
+  const sweet = sweets.find(s => s.id === id);
+
+  if (!sweet) {
+    throw new Error(`Sweet with ID ${id} not found`);
+  }
+
+  // Update only if property exists in newData
+  if (newData.name !== undefined) sweet.name = newData.name;
+  if (newData.category !== undefined) sweet.category = newData.category;
+  if (newData.price !== undefined) sweet.price = newData.price;
+  if (newData.quantity !== undefined) sweet.quantity = newData.quantity;
+
+  return sweet;
+}
+
 module.exports = {
   addSweet,
   deleteSweet,
-  getAllSweets
+  getAllSweets,
+  updateSweet
 };
+
+
 
 
 
